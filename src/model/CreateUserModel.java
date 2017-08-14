e225153dd05acbfd34c84b7af72f6bcfa1cd6770
mbmachado <mateusbarbosa@infojr.com.br>
2017-08-14 13:38:49 -0300

    Muda criação de conta para CreateUserModel.createUser()

--- src/model/CreateUserModel.java
+++ src/model/CreateUserModel.java
@@ -1,9 +1,11 @@
 package model;
 
 import contract.CreateUser;
+import mock.AccountDatabase;
 import mock.Database;
 import mock.UserDatabase;
 import model.pojo.User;
+import model.pojo.Account;
 
 
 public class CreateUserModel implements CreateUser.Model {
@@ -16,10 +18,13 @@ public class CreateUserModel implements CreateUser.Model {
 
     public void createUser(User user) {
         int result;
+        Account account = new Account();
         try {
             if (Database.getData(UserDatabase.PATH_USERS, UserDatabase.INDEX_EMAIL, user.getEmail()) == null) {
                 result = Database.insert(UserDatabase.PATH_USERS, user);
                 user.setId(result);
+                account.setUserId(result);
+                Database.insert(AccountDatabase.PATH_ACCOUNTS, account);         
                 presenter.onCreateSuccess(result);
             } else {
                 presenter.onError("O email já está cadastrado");
--- src/presenter/CreateUserPresenter.java
+++ src/presenter/CreateUserPresenter.java
@@ -1,19 +1,15 @@
 package presenter;
 
-import contract.CreateAccount;
 import contract.CreateUser;
 import model.CreateUserModel;
-import model.pojo.Account;
 import model.pojo.User;
 import util.Validate;
-import view.CreateAccountView;
 import view.HomeView;
 
 
 public class CreateUserPresenter implements CreateUser.Presenter {
 
     private CreateUser.View view;
-    private CreateAccount.View accountView;
     private CreateUser.Model model;
 
     public CreateUserPresenter(CreateUser.View view) {
@@ -38,8 +34,6 @@ public class CreateUserPresenter implements CreateUser.Presenter {
 
     @Override
     public void onCreateSuccess(int user_id) {
-    	accountView = new CreateAccountView();
-    	accountView.createAccount(new Account(), user_id);
     	view.changePanel(new HomeView());
     }
 
