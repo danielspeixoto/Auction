677be64edb6ec07a883de3d219bee2985dbc8024
mbmachado <mateusbarbosa@infojr.com.br>
2017-08-23 02:05:48 -0300

    Modifica cursor quando over a button

--- src/view/component/SimpleButton.java
+++ src/view/component/SimpleButton.java
@@ -1,6 +1,7 @@
 package view.component;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -23,25 +24,29 @@ public class SimpleButton extends JButton {
 			@Override
 			public void mouseExited(MouseEvent e) {
 				setForeground(Color.DARK_GRAY);
-				setBackground(Color.WHITE);	
+				setBackground(Color.WHITE);
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				setForeground(Color.WHITE);
 				setBackground(Color.DARK_GRAY);
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				setForeground(Color.WHITE);
 				setBackground(Color.DARK_GRAY);
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				setForeground(Color.WHITE);
 				setBackground(Color.DARK_GRAY);
+				setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 
 			@Override
